5b9fbd528771f43597bed19e494d7d1f250c6273
CaioNathanS <nathansallenave@gmail.com>
2023-02-27 17:17:48 -0300

    corrige conexao com o banco de dados e corrige nome do projeto

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect('mongodb://127.0.0.1:27017/wbadvocacia', { useNewUrlParser: true, useUnifiedTopology:true })
+    .then(() => console.log("Mongodb connected"))
+    .catch(err => console.log(err));
 
 app.use('/api/users', userRouter);
 app.use('/api/casos', casosRouter);
@@ -41,7 +40,7 @@ app.get('*', (req, res) =>
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
